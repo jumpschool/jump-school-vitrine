fbea331b16c1bec1d2e49449602eda6d804805df
OSMaben <freegiftcardishere@gmail.com>
2024-07-12 12:35:13 +0100

    adding new changes on the next section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import HeroSection from "../Components/Hero/HeroSection";
-
+import BousteCareer from "../Components/bousteVotreCareer/BousteVotreCareer";
 export default function Home(){
     return (
         <>
         <HeroSection />
+        <BousteCareer />
         </>
     )
 }
