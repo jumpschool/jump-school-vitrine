e6db82f1362ec97b9712559df6fa70cef44dc67c
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-07-12 11:53:42 +0100

    Rapport section is done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../Components/Hero/HeroSection";
+import Rapport from "../Components/Home/RapportInsersion/Rapport";
 
 export default function Home(){
     return (
         <>
         <HeroSection />
+        <Rapport/>
         </>
     )
 }
