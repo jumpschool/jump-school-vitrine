f8fcf49fc3545a7ff09b29c32e8f9dcda16f9eac
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-07-24 17:38:55 +0100

    changing the config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.hubapi.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
     host: '0.0.0.0',
     port: 5173
   },
