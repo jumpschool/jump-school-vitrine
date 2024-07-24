5dc462cfe919203d1af93506e08f2d2801721dbd
SalBouhamidi <bouhamidi.sal@gmail.com>
2024-07-24 10:43:23 +0100

    Testing the compo o n versel

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -1,68 +1,74 @@
-import { useRef, useState } from "react";
-import FirstName from './Contactfield/FirstName'
-import LastName from './Contactfield/LastName'
-import Message from "./Contactfield/Message";
-import Email from "./Contactfield/Email";
-import PhoneNumber from "./Contactfield/PhoneNumber";
-import PrivacyPolicy from "./Contactfield/PrivacyPolicy";
-import SendMessage from "./Contactfield/SendMessage";
-import toast, { Toaster } from 'react-hot-toast';
-import decoration from "../../assets/images/decoration.png"
-import React, { useEffect } from 'react';
-import './Herosection.css'
+import React, { useState } from "react";
+import axios from "axios";
 
+export default function ContactForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const data = {
+      properties: [
+        { property: "firstname", value: firstName },
+        { property: "lastname", value: lastName },
+        { property: "email", value: email },
+        { property: "phone", value: phoneNumber },
+        { property: "message", value: message },
+      ],
+    };
 
-
-export default function ContactForm(){
-
-    // const form = useRef();
-    // const [isChecked1, setChecked1] = useState(false);
-
-    // const sendEmail = (e) => {
-    //     e.preventDefault();
-
-    //     if (!isChecked1) {
-    //         toast.error("Veuillez accepter les champs obligatoires pour continuer.");
-    //         return;
-    //     }
-    // }
-    // useEffect(() => {
-    //     const script = document.createElement('script');
-    //     script.src = 'https://js.hsforms.net/forms/v2.js';
-    //     script.async = true;
-    //     script.defer = true;
-    //     document.body.appendChild(script);
-
-    //     // Cleanup function to remove the script after component unmounts
-    //     return () => {
-    //         document.body.removeChild(script);
-    //     }
-    // }, []);
-
-    useEffect(() => {
-        if (window.hbspt) {
-            window.hbspt.forms.create({
-                portalId: '145061004',
-                formId: '842cce6d-c986-4ce6-a12c-2ef003ee631b',
-                target: '#hubspot-form',
-                // inlineMessage: 'Thanks for contacting us!',
-                onFormSubmit: () => toast.success('Form submitted successfully!')
-            });
+    try {
+      const response = await axios.post(
+        `https://api.hubapi.com/contacts/v1/contact/?hapikey=${
+          import.meta.env.VITE_Hubspot_API
+        }`,
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    }, []);
+      );
 
-    return (
-        <>
-    <div className="parent mt-[20%] relative">
-        <img src={decoration} alt="" className="absolute top-[-2%]" />
-      <div className="main_text bg-rose-200 p-[15%]"> 
-         <div id="hubspot-form" className="hubspot-form"></div>
-    </div>
-    </div>
-        
+      if (response.status === 200) {
+        alert("Submission successful");
+      } else {
+        alert("Submission failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Submission failed");
+    }
 
-        </>
-    );
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPhoneNumber("");
+    setMessage("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="mt-[15%]">
+      <input name="firstName" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+      <br />
+
+      <input name="lastName" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+      <br />
+
+      <input name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+      <br />
+
+      <input name="phoneNumber" placeholder="Phone Number" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
+      <br />
+
+      <input name="message" placeholder="Please add information regarding your inquiry" value={message} onChange={(e) => setMessage(e.target.value)}/>
+      <br />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
